89e9d3ed4768991e8217b8236c5ebf8ac665853d
Aatif Shaikh <46748960+aatif24@users.noreply.github.com>
2023-07-13 12:33:47 +0530

    image update

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import logo from '../../public/logo-menu.png'
 export default function Header() {
+let width;
+let height;
     return <div className="h-12 w-full flex">
-        <div className="logo bg-gradient-to-r w-2/6 from-orange-600 to-gray-100">
-            <Image width={100} height={100} className="h-10 w-fit mt-1" src="/logo-menu.png" alt="AirFi-NL" />
+        <div className="logo bg-gradient-to-r w-2/6 from-orange-600 to-gray-100 flex items-center">
+            <Image className="m-1 h-10 w-auto" src={logo} alt="AirFi-NL" />
         </div>
     </div>
 }
